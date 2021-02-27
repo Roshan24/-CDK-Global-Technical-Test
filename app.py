f1ae29112f1c8e32ea8ba620274b1ec8b49d2fc1
Roshan24 <45001551+Roshan24@users.noreply.github.com>
2021-02-27 18:22:29 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask, request, url_for
+from flask import Flask, render_template, request, url_for
 import json
 import datetime
 
 app = Flask(__name__)
 
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/', methods=['GET','POST'])
 def home():
     newComment = {}
     with open('cases.json', 'r') as jFile:
@@ -17,11 +17,16 @@ def home():
         if request.form['comment'] != "" and request.form['comment'] != None:
             newComment['CommentBody'] = request.form['comment']
             newComment['CommentDate'] = datetime.datetime.now().isoformat()
+            newComment['CreatedBy'] = 'john test'
+            newComment['Id'] = "c-"
 
             items['Comments'].append(newComment)
+            items['LastModifiedDate'] = datetime.datetime.now().isoformat()
+
             data[0] = items
 
             with open('cases.json', 'w') as outfile:
                 json.dump(data, outfile)
                 outfile.close()
-    return
+
+    return render_template("home.html", items=items)
